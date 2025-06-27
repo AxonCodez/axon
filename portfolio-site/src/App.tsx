0014397574a7ec064a30e3e7ac06a65ad7a99793
AxonCodez <92618846+AxonCodez@users.noreply.github.com>
2025-06-27 17:05:31 +0000

    update

---
+++ portfolio-site/src/App.tsx
@@ -0,0 +1,11 @@
+import { SplineSceneBasic } from "@/components/ui/demo"
+
+function App() {
+  return (
+    <main className="min-h-screen bg-black text-white p-4">
+      <SplineSceneBasic />
+    </main>
+  )
+}
+
+export default App
